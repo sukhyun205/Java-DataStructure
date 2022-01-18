9b2ffa8106288cc9a389ad7dbaa63a051c3a005c
Sukhyun Kwon <sukhyun205@gmail.com>
2022-01-19 02:35:16 +0900

    20220119 save

--- 220119 Java algorithm/src/Main.java
+++ 220119 Java algorithm/src/Main.java
@@ -1,15 +1,22 @@
+import java.util.Scanner;
+
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args){
 		// TODO Auto-generated method stub
+
+		System.out.print((char)126);
 		
-		System.out.println("\\     /\\");
-		System.out.println(" )   ( ')");
-		System.out.println("(   /  )");
-		System.out.println(" \\(__)|");
+		System.out.print("a");
+		
+		
 		
 
+
+		
 	}
 
 }
+
+
---
+++ 220119 Java algorithm/src/Storage.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class Storage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+
+	}
+
+}
+
+/* Used BufferedWriter
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		bw.write("Hello World!");
+		bw.flush();
+		bw.close();
+	}
+
+}
+*/
+
+/* 10171
+System.out.println("\\     /\\");
+System.out.println(" )   ( ')");
+System.out.println("(   /  )");
+System.out.println(" \\(__)|");
+*/
+/* 1000
+Scanner sc = new Scanner(System.in);
+
+String A = sc.next();
+String B = sc.nextLine(); //공백을 포함하는 문자열을 쓸때는 nextLine();을 사용		
+System.out.println(A);
+System.out.println(B);
+
+sc.close();
+*/
